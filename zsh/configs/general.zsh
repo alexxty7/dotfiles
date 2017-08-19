83bee9eea253b0c42f96d35e3bc6b58f1aaf67c5
Alexander Katulskiy <alexxty7@gmail.com>
2017-08-19 22:59:45 +0300

    Set zsh completion to case-insensitive mode

--- zsh/configs/general.zsh
+++ zsh/configs/general.zsh
@@ -10,6 +10,10 @@ SAVEHIST=20000
 autoload -U compinit
 compinit
 
+## case-insensitive (all), partial-word and then substring completion
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' \
+    'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
 # Disable flow control commands (keeps C-s from freezing everything)
 stty start undef
 stty stop undef
