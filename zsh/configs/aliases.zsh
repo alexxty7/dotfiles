922175a0b7e54137a9e73ef6f6fb4de165a87c94
Alexander Katulskiy <alexxty7@gmail.com>
2017-08-19 12:02:16 +0300

    Update alias config

--- zsh/configs/aliases.zsh
+++ zsh/configs/aliases.zsh
@@ -1,2 +1,18 @@
+# General
 alias c=clear
 alias y=yarn
+
+# Git
+alias gp='git push'
+alias gcmsg='git commit -m'
+
+# Bundler
+alias b="bundle"
+alias bi='bundle install'
+
+# Rails
+alias rdm='rake db:migrate'
+alias rs='rails s'
+
+# Pretty print the path
+alias path='echo $PATH | tr -s ":" "\n"'
